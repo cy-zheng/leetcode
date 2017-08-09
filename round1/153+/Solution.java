624fdfb49b85cf34cbe2f2eb812c1669d13f7b09
zcy <zcy19941015@qq.com>
2017-08-09 23:34:45 +0800

    17.8.9

---
+++ round1/153+/Solution.java
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left + 1 < right) {
+            mid = (right - left) / 2 + left;
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+            else {
+                if (nums[mid] > nums[left]) {
+                    left = mid;
+                }
+                else {
+                    right = mid;
+                }
+            }
+        }
+        
+        if (nums[left] < nums[right])
+            return nums[left];
+        else
+            return nums[right];
+            
+    }
+}
