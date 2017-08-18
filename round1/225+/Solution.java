780e29e819e526fa07ba862554f96c1389fab3f3
zhengchenyu <zcy19941015@qq.com>
2017-08-18 17:01:11 +0800

    17.8.18

---
+++ round1/225+/Solution.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class MyStack {
+    
+    private boolean flag;
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q1.offer(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        int result;
+        while (q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+        result = q1.poll();
+        while (q2.size() > 0) {
+            q1.offer(q2.poll());
+        }
+        return result;
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        int result;
+        while (q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+        result = q1.poll();
+        while (q2.size() > 0) {
+            q1.offer(q2.poll());
+        }
+        q1.offer(result);
+        return result;
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.size() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
